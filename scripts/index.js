fc7a8fec47bc41a2c06679f361f330493057bc25
Fahim047 <fahimulislam58@gmail.com>
2024-10-07 08:30:16 +0600

    ✅ Adopt pet feature done

--- scripts/index.js
+++ scripts/index.js
@@ -52,7 +52,7 @@ const displayPets = (pets) => {
       src="./images/icons/breed.svg"
       alt=""
     />
-    <span>Breed: ${breed ? breed : 'N/A'}</span>
+    <span>Breed: ${breed || 'N/A'}</span>
   </div>
   <div class="flex items-center gap-2">
     <img
@@ -60,7 +60,7 @@ const displayPets = (pets) => {
       src="./images/icons/calender.svg"
       alt=""
     />
-    <span>Birth: ${date_of_birth ? date_of_birth : 'N/A'}</span>
+    <span>Birth: ${date_of_birth || 'N/A'}</span>
   </div>
   <div class="flex items-center gap-2">
     <img
@@ -68,7 +68,7 @@ const displayPets = (pets) => {
       src="./images/icons/gender.svg"
       alt=""
     />
-    <span>Gender: ${gender ? gender : 'N/A'}</span>
+    <span>Gender: ${gender || 'N/A'}</span>
   </div>
   <div class="flex items-center gap-2">
     <img
@@ -82,8 +82,8 @@ const displayPets = (pets) => {
 <div class="flex justify-between gap-1 border-t pt-4">
   <button class="btn btn-sm like-btn">
   <img src="./images/icons/like.svg" alt="" /></button>
-  <button class="btn btn-sm text-[#0E7A81]">Adopt</button>
-  <button class="btn btn-sm text-[#0E7A81]">Details</button>
+  <button class="adopt-btn btn btn-sm text-[#0E7A81]">Adopt</button>
+  <button class="details-btn btn btn-sm text-[#0E7A81]">Details</button>
 </div>
     `;
 
@@ -91,6 +91,10 @@ const displayPets = (pets) => {
 
 		const likeBtn = div.querySelector('.like-btn');
 		likeBtn.addEventListener('click', () => addToFavorite(image));
+		const detailsBtn = div.querySelector('.details-btn');
+		detailsBtn.addEventListener('click', () => showDetails(item));
+		const adoptBtn = div.querySelector('.adopt-btn');
+		adoptBtn.addEventListener('click', () => adoptPet());
 	});
 };
 
@@ -108,6 +112,75 @@ const addToFavorite = (imageUrl) => {
 	const img = document.createElement('img');
 	img.src = imageUrl;
 	favoritePetContainer.appendChild(img);
+};
+const showDetails = (item) => {
+	const { pet_name, image, breed, date_of_birth, price, gender } = item;
+	const detailsModal = document.getElementById('details-modal');
+	const modalContent = detailsModal.querySelector('.modal-content');
+	modalContent.innerHTML = `
+<img class="pet-image w-full rounded-md" src=${image} alt=${breed} />
+<h4 class="font-extrabold text-2xl">${pet_name}</h4>
+<div class="space-y-1 grid grid-cols-2 gap-2 text-gray-500 text-sm">
+  <div class="flex items-center gap-2">
+    <img
+      class="size-4"
+      src="./images/icons/breed.svg"
+      alt=""
+    />
+    <span>Breed: ${breed || 'N/A'}</span>
+  </div>
+  <div class="flex items-center gap-2">
+    <img
+      class="size-4"
+      src="./images/icons/calender.svg"
+      alt=""
+    />
+    <span>Birth: ${date_of_birth || 'N/A'}</span>
+  </div>
+  <div class="flex items-center gap-2">
+    <img
+      class="size-4"
+      src="./images/icons/gender.svg"
+      alt=""
+    />
+    <span>Gender: ${gender || 'N/A'}</span>
+  </div>
+  <div class="flex items-center gap-2">
+    <img
+      class="size-4"
+      src="./images/icons/dollar.svg"
+      alt=""
+    />
+    <span>Price : ${price ? price + '$' : 'N/A'}</span>
+  </div>
+</div>
+<div class="border-t py-4 space-y-2">
+	<h4 class="font-bold">Details Information</h4>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+</div>
+	`;
+	detailsModal.showModal();
+};
+const adoptPet = () => {
+	const adoptModal = document.getElementById('adopt-modal');
+	const modalContent = adoptModal.querySelector('.modal-content');
+	modalContent.innerHTML = `
+		<h2 class="text-3xl">Congratulations!</h2>
+		<h3 id="counter" class="text-2xl">3</h3>
+		<p>You have successfully adopted the pet.</p>
+	`;
+	adoptModal.showModal();
+	const timer = setInterval(() => {
+		const counter = adoptModal.querySelector('#counter');
+		counter.innerText = Number(counter.innerText) - 1;
+
+		if (counter.innerText === '0') {
+			clearInterval(timer);
+			counter.innerText = '1';
+			adoptModal.close();
+		}
+	}, 1000);
+	event.target.disabled = true;
 };
 
 loadCategories();
